7bfa432e3064e939ca6f1c073ba4ada6393413cc
knightvertrag <amiapu1@gmail.com>
2020-07-17 20:33:07 +0530

    Added validation route to blockchain

--- blockchain.py
+++ blockchain.py
@@ -89,4 +89,14 @@ def get_chain():
     return jsonify(response), 200
 
 
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'The Blockchain is valid.'}
+    else:
+        response = {'message': 'The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+
 app.run(host='0.0.0.0', port=5000)
