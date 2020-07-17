af96bfe7113dc4c2dff20932022e13edffce7f8c
knightvertrag <amiapu1@gmail.com>
2020-07-17 21:19:25 +0530

    Added GET route to replace chain by longest chain

--- coin.py
+++ coin.py
@@ -169,4 +169,20 @@ def connect_node():
     return jsonify(response), 201
 
 
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {
+            'message': 'Chain replaced by longest chain.',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Chain not replaced',
+            'new_chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+
 app.run(host='0.0.0.0', port=5000)
